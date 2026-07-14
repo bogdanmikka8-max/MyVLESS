f029356ff0115a014913f14b59a5d77afe20fed5
bogdanmikka8-max <bogdanmikka8@gmail.com>
2026-07-15 00:40:48 +0400

    Create MyVLESSApplication.kt

---
+++ app/src/main/java/com/myvless/app/MyVLESSApplication.kt
@@ -0,0 +1,10 @@
+package com.bogdanmikka.myvless
+
+import android.app.Application
+
+class MyVLESSApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // Здесь можно инициализировать базы данных, preferences и т.д.
+    }
+}
